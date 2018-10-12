62e1999cd42249b0907409249dcd04c28aada59d
ketangote <ketan.gote@metamagic.in>
2018-10-12 12:42:47 +0530

    Added timeout service

--- src/main/java/com/metamagic/productreviewms/api/ProductReviewAPI.java
+++ src/main/java/com/metamagic/productreviewms/api/ProductReviewAPI.java
@@ -25,8 +25,9 @@ public class ProductReviewAPI {
 	
 
 	@GetMapping(value = "/timeout/{id}")
-	public ResponseEntity<Collection<ProductReview>> reviewByIdAndTimeout(@PathVariable("id") String id){
+	public ResponseEntity<Collection<ProductReview>> reviewByIdAndTimeout(@PathVariable("id") String id) throws InterruptedException{
 		System.out.println("Product Review reviewById  "+new Date());
+		Thread.sleep(50000);
 		return new ResponseEntity<Collection<ProductReview>>(ProductReviewService.getInstance().getProductReview(id), HttpStatus.OK);
 	}
 
