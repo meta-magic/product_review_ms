9a26444514cec0489c10df1b7cf646b2238d5281
ketangote <ketan.gote@metamagic.in>
2018-10-17 14:51:35 +0530

    UPDATED Jaeger URL

--- src/main/java/com/metamagic/productreviewms/ProductReviewMsApplication.java
+++ src/main/java/com/metamagic/productreviewms/ProductReviewMsApplication.java
@@ -7,8 +7,16 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean;
 
 import com.metamagic.productreviewms.dto.VersionInfo;
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ProbabilisticSampler;
+import com.uber.jaeger.Tracer.Builder;
+import com.uber.jaeger.metrics.Metrics;
+import com.uber.jaeger.metrics.NullStatsReporter;
+import com.uber.jaeger.metrics.StatsFactoryImpl;
+import com.uber.jaeger.propagation.b3.B3TextMapCodec;
+import com.uber.jaeger.reporters.RemoteReporter;
+import com.uber.jaeger.samplers.ConstSampler;
+import com.uber.jaeger.senders.HttpSender;
+
+import io.opentracing.propagation.Format.Builtin;
 
 @SpringBootApplication
 public class ProductReviewMsApplication extends SpringBootServletInitializer{
@@ -34,11 +42,15 @@ public class ProductReviewMsApplication extends SpringBootServletInitializer{
 	}
 	
 	@Bean
-	public io.opentracing.Tracer jaegerTracer() {
-	    return new Configuration("productreviewms", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-	        new Configuration.ReporterConfiguration())
-	        .getTracer();
-	    }
+	  public io.opentracing.Tracer jaegerTracer() {
+	    Builder builder = new Builder("spring-boot",
+	        new RemoteReporter(new HttpSender("http://jaeger-collector.istio-system:14268/api/traces"), 10,
+	        65000, new Metrics(new StatsFactoryImpl(new NullStatsReporter()))),
+	        new ConstSampler(true))
+	        .registerInjector(Builtin.HTTP_HEADERS, new B3TextMapCodec())
+	        .registerExtractor(Builtin.HTTP_HEADERS, new B3TextMapCodec());
+	    return builder.build();
+	  }
 	
 
 }
